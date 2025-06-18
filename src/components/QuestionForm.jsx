7211462ef0a2621a29b4366453c5bb842fb4bc75
Taylor Putman <fishputman@gmail.com>
2025-06-18 13:08:52 -0400

    added htmldecode function instead of dangerousinnerHtml

--- src/components/QuestionForm.jsx
+++ src/components/QuestionForm.jsx
@@ -2,26 +2,40 @@ import { useState } from "react";
 
 const QuestionForm = ({ questions }) => {
   const [answers, setAnswers] = useState({});
+
+  const handleSelectAnswer = (questionIndex, selectedAnswer) => {
+    setAnswers((prev) => ({
+      ...prev,
+      [questionIndex]: selectedAnswer,
+    }));
+  };
+
+  // Utility to decode HTML entities to plain text
+  const decodeHtml = (html) => {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  };
+
   return (
     <form>
       <ul>
         {questions.results.map((item, index) => (
           <li key={index}>
-            <p dangerouslySetInnerHTML={{ __html: item.question }} />
+            <p>{decodeHtml(item.question)}</p>
             {item.shuffledAnswers.map((answer) => {
               const id = `${index}-${answer}`;
               return (
                 <div key={id}>
                   <input
+                    required
                     type="radio"
                     id={id}
-                    name={`question-${index}`}
+                    name={`question-${index}`} // Group by question
                     value={answer}
+                    onChange={() => handleSelectAnswer(index, answer)}
                   />
-                  <label
-                    htmlFor={id}
-                    dangerouslySetInnerHTML={{ __html: answer }}
-                  />
+                  <label htmlFor={id}>{decodeHtml(answer)}</label>
                 </div>
               );
             })}
