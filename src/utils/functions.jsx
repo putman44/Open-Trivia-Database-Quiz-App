185afdddc6c3a1f79fa2f250bfa64f76386d5b97
Taylor Putman <fishputman@gmail.com>
2025-06-23 12:24:51 -0400

    moved shuffleArray function

--- src/utils/functions.jsx
+++ src/utils/functions.jsx
@@ -3,3 +3,7 @@ export const decodeHtml = (html) => {
   txt.innerHTML = html;
   return txt.value;
 };
+
+// Utility function to shuffle an array (Fisher-Yates-style randomness)
+export const shuffleArray = (array) =>
+  [...array].sort(() => Math.random() - 0.5);
